2b3ee6c7aa90da03ab025f96c8f054442955ff09
AntonioAndreocci <andreocci.antonio10@gmail.com>
2021-10-13 15:55:11 +0200

    c

---
+++ AnimaleCQRS/AnimaleCQRS/Server/Queries/Handler/VeterinarioQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AnimaleCQRS.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimaleCQRS.Server.Queries.Handler
+{
+    public class VeterinarioQueryHandler
+    {
+
+
+        private readonly VeterinarioDbContext _context;
+
+        public VeterinarioQueryHandler(VeterinarioDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
